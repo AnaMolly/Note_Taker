eb668b46b36290eccd077996a83ef3a7b20e77dc
AnaMolly <analia.mohamdally@gmail.com>
2021-06-17 22:03:45 -0400

    added a server.js file and added paths and get routes. also initialized packages

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data))
+    })
+})
+
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
